72b951ce340f2e837d8f24086a8668bdee55bf3c
ZachBrunker-2000 <brunkerz@gmail.com>
2025-05-27 01:52:00 -0400

    Created the player class along with implementing forward and backward movement. Also added containers and groups to contain the player objects.

--- main.py
+++ main.py
@@ -3,21 +3,36 @@
 # throughout this file
 import pygame
 from constants import *
+from Player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
+            
         screen.fill((0,0,0))
+        
+        updatable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
+        
+        clock.tick(60)
+        dt = clock.tick(60)/1000
         pygame.display.flip()
         
         
